01d42f1c4f52c054f85d4044f073beae24b27af1
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-22 06:55:51 +0100

    beta  font tracking widened

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ const Home: NextPage = () => {
         <div className="container flex flex-grow flex-col items-center gap-8 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             WPRS <span className="text-[hsl(125,50%,56%)]">Forecast</span>{" "}
-            <span className="text-sm">beta</span>
+            <span className="text-sm tracking-normal">beta</span>
           </h1>
           <form
             className="mt-5 w-full justify-center gap-3 sm:flex"
