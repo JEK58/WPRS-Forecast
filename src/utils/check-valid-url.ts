b79480e062386c7ae85683d6c21d5ecb6dc204b7
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-22 18:59:55 +0100

    swissleague.ch added and CIVL pilot lookup refined

--- src/utils/check-valid-url.ts
+++ src/utils/check-valid-url.ts
@@ -2,6 +2,7 @@ export function isValidUrl(url: string) {
   return (
     url.includes("airtribune.com/") ||
     url.includes("civlcomps.org/event/") ||
-    url.includes("pwca.org/events/")
+    url.includes("pwca.org/events/") ||
+    url.includes("swissleague.ch/comp-league/competitions")
   );
 }
---
+++ src/utils/get-swissleague-pilots.ts
@@ -0,0 +1,52 @@
+import { load } from "cheerio";
+import { lookupCivlId } from "@/utils/lookup-civl-id";
+
+export async function getSwissleaguePilots(url: string) {
+  const response = await fetch(url);
+  const body = await response.text();
+
+  const $ = load(body, { xmlMode: true });
+
+  const content = $(".mwc-datatable");
+
+  const rows = content.find("tr");
+
+  interface RowData {
+    [key: string]: string;
+  }
+
+  const data: RowData[] = [];
+
+  rows.each((_, row) => {
+    const columns = $(row).find("td");
+    const rowData: RowData = {};
+
+    columns.each((j, column) => {
+      const columnName = content.find("th").eq(j).text().trim().toLowerCase();
+      rowData[columnName] = $(column).text().trim();
+    });
+
+    data.push(rowData);
+  });
+
+  const confirmedPilots = data.filter((el) => {
+    return el.status == "Paid";
+  });
+
+  const pilots = await Promise.all(
+    confirmedPilots.map(async (el) => {
+      const name = el.pilot ?? "";
+      const civlID = await lookupCivlId(name);
+
+      return {
+        name,
+        nationality: el.country,
+        civlID,
+        wing: el.glider,
+        status: el.status,
+      };
+    })
+  );
+
+  return pilots;
+}
