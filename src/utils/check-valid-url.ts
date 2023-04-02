382b39da3ae98b29315e5ed677c606980059e99a
Stephan Schöpe <socialcrap+github@posteo.de>
2023-04-02 16:41:32 +0200

    validate url scheme on client

--- src/utils/check-valid-url.ts
+++ src/utils/check-valid-url.ts
@@ -1,8 +1,15 @@
 export function isValidUrl(url: string) {
+  if (!validateHttpsLink(url)) return false;
   return (
     url.includes("airtribune.com/") ||
     url.includes("civlcomps.org/event/") ||
     url.includes("pwca.org/events/") ||
     url.includes("swissleague.ch/comp-league/competitions")
   );
+}
+
+function validateHttpsLink(link: string) {
+  const regex =
+    /^https:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  return regex.test(link);
 }
