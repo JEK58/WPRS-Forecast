9780f46a99099d7995a4c5b440bd56cd1db74e67
Stephan Schöpe <socialcrap+github@posteo.de>
2023-05-06 12:22:59 +0200

    better table formatting and filtering of null wprs

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -15,8 +15,9 @@ const Stats = (
     if (!stat.compTitle) return;
     return (
       <tr key={stat.id}>
-        <td className="text-right">{stat.wprs} -</td>
-        <td>
+        <td className="text-right align-top">{stat.wprs}</td>
+        <td className="align-top">-</td>
+        <td className="max-w-md whitespace-pre-line">
           <a
             className="text-[hsl(125,50%,56%)] hover:underline hover:decoration-dotted"
             href={sanitizeUrl(stat.compUrl)}
@@ -37,7 +38,9 @@ const Stats = (
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-grow flex-col items-center gap-8 px-4 py-16 ">
-          <h1 className="text-3xl tracking-tight text-white ">Statistics</h1>
+          <h1 className="text-3xl tracking-tight text-white ">
+            Recent Queries
+          </h1>
 
           <table className="table-auto text-white">
             <tbody>{listStats}</tbody>
@@ -60,8 +63,12 @@ export const getServerSideProps = async () => {
   try {
     const data = await prisma.usage.findMany({
       orderBy: { createdAt: "desc" },
+      where: {
+        wprs: { not: null },
+        compTitle: { not: null },
+      },
       select: { wprs: true, compUrl: true, id: true, compTitle: true },
-      take: 18,
+      take: 20,
     });
     return {
       props: { data },
