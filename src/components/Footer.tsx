644aceef470c9873ff3f5977b6714d5c7969dbc3
Stephan Schöpe <socialcrap+github@posteo.de>
2023-06-05 21:00:21 +0200

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,25 @@ import { FaGithub } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer className="mb-3 flex gap-2 p-2 text-sm text-gray-400 dark:text-slate-400">
+    <footer className="mb-3 p-2 text-xs  text-gray-400 dark:text-slate-400">
+      <div className="mb-2 flex items-center  justify-center gap-2">
+        <a
+          href="https://www.stephanschoepe.de/impressum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Impressum
+        </a>
+        <span>|</span>
+        <Link href="/stats">Recent queries</Link>
+      </div>
       <a
         href="https://github.com/JEK58/wprs-forecast"
-        className="flex items-center justify-center text-xs"
+        className="flex items-center"
       >
         <FaGithub className="mr-1" />
         Made with ❤️ by Stephan Schöpe
       </a>
-      |
-      <a
-        href="https://www.stephanschoepe.de/impressum"
-        className="flex items-center justify-center text-xs"
-      >
-        Impressum
-      </a>
-      |
-      <Link className="flex items-center justify-center text-xs" href="/stats">
-        Statistics
-      </Link>
     </footer>
   );
 }
