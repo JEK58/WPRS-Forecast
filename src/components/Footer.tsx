035b3ee9ab1565ccb93f24e10a1d68580e60b95e
Stephan Schöpe <socialcrap+github@posteo.de>
2023-06-05 21:00:39 +0200

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export function Footer() {
   return (
     <footer className="mb-3 p-2 text-xs  text-gray-400 dark:text-slate-400">
-      <div className="mb-2 flex items-center  justify-center gap-2">
+      <div className="mb-2 flex items-center justify-center gap-2">
         <a
           href="https://www.stephanschoepe.de/impressum"
           target="_blank"
