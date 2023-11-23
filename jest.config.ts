aa3bba688c1b707aa6902ea75002d54fb93a3aba
Stephan Schöpe <socialcrap+github@posteo.de>
2023-11-23 14:47:53 +0100

    wip

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig: Config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   // testEnvironment: "jest-environment-jsdom",
 };
 
