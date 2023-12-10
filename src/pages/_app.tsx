f88feb7fa991b5458ead8c289504fdcd12ef0662
Stephan Schöpe <socialcrap+github@posteo.de>
2023-12-10 19:21:42 +0100

    plausible added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import { api } from "@/utils/api";
+import PlausibleProvider from "next-plausible";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider
+      domain="wprs-forecast.org"
+      selfHosted={true}
+      customDomain="https://plausible.lurb.org"
+    >
+      <Component {...pageProps} />;
+    </PlausibleProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
