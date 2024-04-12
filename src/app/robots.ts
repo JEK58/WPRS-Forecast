39f9d8ace98815af92bc1fdbf2ab2575fd860c08
Stephan Schöpe <socialcrap+github@posteo.de>
2024-04-12 10:39:07 +0200

    robots.txt updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,11 @@
-import { MetadataRoute } from "next";
+import { type MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/forecast/",
+      disallow: "/forecast",
     },
   };
 }
