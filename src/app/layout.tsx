3c2852dd1400520f7a3897e1945f1a1b597144f1
Stephan Schöpe <socialcrap+github@posteo.de>
2024-05-02 22:21:00 +0200

    Alert deactivated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,15 +37,14 @@ export default function RootLayout({
       <body className={`font-sans ${inter.variable}`}>
         <div className="flex min-h-screen flex-col bg-gradient-to-r from-green-400 to-blue-500 pb-4">
           <div className="flex flex-col items-center justify-center space-y-8 px-0 py-4 md:px-4">
-           
             <Link href="/">
               <h1 className="mt-6 text-6xl font-bold text-white sm:text-center sm:text-[5rem] sm:leading-tight">
                 WPRS
                 <span className="block text-green-300 sm:inline">Forecast</span>
               </h1>
             </Link>
-             {/* Alert */}
-             <div className="px-3 md:max-w-3xl">
+            {/* Alert */}
+            {/* <div className="px-3 md:max-w-3xl">
               <div role="alert" className="alert alert-error">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -67,7 +66,7 @@ export default function RootLayout({
                   109 pilots will be to high.
                 </span>
               </div>
-            </div>
+            </div> */}
             {children}
           </div>
           <Footer />
