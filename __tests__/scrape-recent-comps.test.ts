4029f479be73a44356ebd83260f7df1136e2392c
Stephan Schöpe <socialcrap+github@posteo.de>
2024-05-09 17:55:42 +0200

    console.log removed

--- __tests__/scrape-recent-comps.test.ts
+++ __tests__/scrape-recent-comps.test.ts
@@ -4,7 +4,6 @@ import { scrapeRecentCivlComps } from "../src/utils/update-recent-comps";
 describe("Scrape recent comps from CIVL website", () => {
   it("should find the recent comps in the world ranking", async () => {
     const comps = await scrapeRecentCivlComps();
-    console.log("🚀 ~ comps:", comps);
     expect(comps.length).toBeGreaterThan(100);
     expect(comps[0]?.pilots).toBeGreaterThan(30);
     expect(comps[0]?.winnerScore).toBeGreaterThan(10);
