0baf86abc12cf3b4627cbe8577b69a626984853a
Stephan Schöpe <socialcrap+github@posteo.de>
2024-10-21 20:41:19 +0200

    redis logs added

--- src/server/cache/redis.ts
+++ src/server/cache/redis.ts
@@ -2,3 +2,11 @@ import Redis from "ioredis";
 import { env } from "@/env.js";
 
 export const redis = new Redis({ host: env.REDIS_URL });
+
+redis.on("connect", () => {
+  console.log("Redis connection established");
+});
+
+redis.on("end", () => {
+  console.log("Redis connection closed");
+});
