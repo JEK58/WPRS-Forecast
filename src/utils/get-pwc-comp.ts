8174e33a63eb9427af3b75d25f23985214571a0f
Stephan Schöpe <socialcrap+github@posteo.de>
2025-02-23 11:11:03 +0100

    pwc 2025 details

--- src/utils/get-pwc-comp.ts
+++ src/utils/get-pwc-comp.ts
@@ -1,6 +1,4 @@
 import { getCivlIds, CIVL_PLACEHOLDER_ID } from "@/utils/get-civl-ids";
-import { load } from "cheerio";
-import { getStartAndEndDateFromRange } from "./get-start-and-end-date-from-range";
 import { getPosition } from "@/utils/utils";
 
 interface PWCApiResponse {
@@ -42,29 +40,18 @@ export async function getPwcComp(url: string) {
   const compUrl = await generatePwcCompUrl(url);
   if (!compUrl) return;
 
-  const response = await fetch(compUrl, { cache: "no-store" });
-  const body = await response.text();
+  // const response = await fetch(compUrl, { cache: "no-store" });
+  // const body = await response.text();
 
-  const $ = load(body, { xmlMode: true });
-  let compTitle = $("div.section-heading h2.title")
-    .first()
-    .text()
-    .replace("Paragliding World Cup", "PWC")
-    .replaceAll(",", " ")
-    .trim();
+  // const $ = load(body, { xmlMode: true });
 
-  let compDate = $("div.information div").first().text();
+  const details = getPwcDetails(url);
 
-  if (compUrl.includes("super-final")) {
-    compTitle =
-      "14th Paragliding World Cup Superfinal 2024 Colombia, Roldanillo";
-    compDate = "04.02.2025 - 15.02.2025";
-  }
+  // const dates = await getStartAndEndDateFromRange(compDate);
 
-  const dates = await getStartAndEndDateFromRange(compDate);
-
-  const startDate = dates?.startDate;
-  const endDate = dates?.endDate;
+  const compTitle = details?.name;
+  const startDate = new Date(details?.startDate ?? "");
+  const endDate = new Date(details?.endDate ?? "");
 
   const apiUrl = getApiUrl(compUrl);
   if (!apiUrl) return;
@@ -154,6 +141,46 @@ function getApiUrl(url: string) {
     return "https://pwca.org/api/events/2025-paragliding-world-cup-turkey-aksaray-2025/selection";
   if (url.includes("pwca-asian-tour-india-bir-2024"))
     return "https://pwca.org/api/events/2024-pwca-asian-tour-india-bir-2024/selection";
+  else {
+    console.log("❌ API URLs need to be updated manually");
+  }
+}
+
+function getPwcDetails(url: string) {
+  // if (url.includes("15th-paragliding-world-cup-super"))
+  //   return "https://pwca.org/api/events/2024-14th-paragliding-world-cup-superfinal-2024-colombia-roldanillo/selection";
+  if (url.includes("world-cup-spain-algodonales"))
+    return {
+      name: "PWC Spain, Algodonales 2025",
+      startDate: "2025-05-10",
+      endDate: "2025-05-17",
+    };
+  if (url.includes("world-cup-china-linzhou-2025"))
+    return {
+      name: "PWC China, Linzhou 2025",
+      startDate: "2025-06-07",
+      endDate: "2025-06-14",
+    };
+  if (url.includes("world-cup-italy-feltre-2025"))
+    return {
+      name: "PWC Italy, Feltre - 40th Guarnieri International Trophy",
+      startDate: "2025-06-28",
+      endDate: "2025-07-05",
+    };
+  if (url.includes("world-cup-macedonia-krushevo-2025"))
+    return {
+      name: "PWC Macedonia, Krushevo 2025",
+      startDate: "2025-07-20",
+      endDate: "2025-07-27",
+    };
+  if (url.includes("world-cup-turkey-aksaray-2025"))
+    return {
+      name: "PWC Turkey, Aksaray 2025",
+      startDate: "2025-08-16",
+      endDate: "2025-08-23",
+    };
+  // if (url.includes("pwca-asian-tour-india-bir-2024"))
+  //   return "https://pwca.org/api/events/2024-pwca-asian-tour-india-bir-2024/selection";
   else {
     console.log("❌ API URLs need to be updated manually");
   }
